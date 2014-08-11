70c9c5bb922f28178c173b90907c8d80fda25ca8
Jeongseok Lee <jslee02@gmail.com>
2014-08-10 23:36:38 -0400

    Adding cicular motion test

--- unittests/testAccuracy.cpp
+++ unittests/testAccuracy.cpp
@@ -128,6 +128,31 @@ public:
     int a = 10;
   }
 
+  void initCircular()
+  {
+    mG = Eigen::Matrix6d::Identity();
+    mW = Eigen::Isometry3d::Identity();
+    mV << 0.0, -DART_PI/5.0, 0.0, 0.0, 0.0, DART_PI/5.0;
+//    mV << 0.0, 0.0, 0.0, 0.1, 0.4, 0.9;
+    mA.setZero();
+    mTimeStep = 0.001;
+  }
+
+  void updateVelocityCircular()
+  {
+    Eigen::Vector3d pWorldOld = mW.translation();
+
+//    mV = mG.inverse() * math::dAdT(expMap2(/*mTimeStep * */mV), mG * mV);
+    mV = mG.inverse() * math::dAdT(integrate(mV, 0.001), mG * mV);
+    mW = mW * integrate(mV, 0.001);
+
+    Eigen::Vector3d vWorld = mW.linear() * mV.tail<3>();
+
+    Eigen::Vector3d pWorld = mW.translation();
+    Eigen::Vector3d pWorld2 = mW.linear().inverse() * mW.translation();
+    int a = 10;
+  }
+
   Eigen::Matrix6d mG;
 
   Eigen::Isometry3d mW;
@@ -168,6 +193,14 @@ class AccuracyTest : public ::testing::Test
 public:
   // Spawn a single box and record accuracy for momentum and energy conservation
   void Boxes(double _dt,
+             int _iterations,
+             int _boxCount,
+             bool _gravity,
+             bool _collision,
+             bool _linear);
+
+  // Spawn a single box and record accuracy for momentum and energy conservation
+  void Boxes2(double _dt,
              int _iterations,
              int _boxCount,
              bool _gravity,
@@ -330,9 +363,158 @@ void AccuracyTest::Boxes(double _dt,
 }
 
 //==============================================================================
+void AccuracyTest::Boxes2(double _dt,
+                         int _iterations,
+                         int _boxCount,
+                         bool _gravity,
+                         bool _collision,
+                         bool _linear)
+{
+  World* world = new World();
+
+  // get gravity value
+  if (_gravity)
+    world->setGravity(Vector3d(0.0, 0.0, -9.81));
+  else
+    world->setGravity(Vector3d::Zero());
+
+  Eigen::Vector3d g = world->getGravity();
+
+  // Box size
+  const double dx = 0.1;
+  const double dy = 0.4;
+  const double dz = 0.9;
+  const double mass = 10.0;
+  // inertia matrix, recompute if the above change
+  const double Ixx = 0.80833333;
+  const double Iyy = 0.68333333;
+  const double Izz = 0.14166667;
+  const Matrix3d I0 = Vector3d(Ixx, Iyy, Izz).asDiagonal();
+
+  // Create box with inertia based on box of uniform density
+  Skeleton* boxSkel = new Skeleton();
+  BodyNode* box = new BodyNode();
+  BoxShape* boxVizShape = new BoxShape(Vector3d(dx, dy, dz));
+  BoxShape* boxColShape = new BoxShape(Vector3d(dx, dy, dz));
+  FreeJoint* boxJoint = new FreeJoint();
+  box->addVisualizationShape(boxVizShape);
+  box->addCollisionShape(boxColShape);
+  box->setParentJoint(boxJoint);
+  box->setMomentOfInertia(Ixx, Iyy, Izz);
+  box->setMass(mass);
+  boxSkel->addBodyNode(box);
+  world->addSkeleton(boxSkel);
+
+  // initial linear velocity in global frame
+  const Vector3d v0(0.0, -1.0, 0.0);
+
+  // initial angular velocity in global frame
+  Vector3d w0(0.0, 0.0, DART_PI/5.0);
+
+  // initial energy value
+  double E0;
+
+
+  for (int i = 0; i < 1; ++i)
+  {
+    Skeleton* skel = world->getSkeleton(i);
+    BodyNode* bodyNode = skel->getBodyNode(0);
+
+    // give models unique names
+    skel->setName("model");
+
+    // give models unique positions
+    setPosition(skel, Vector3d(1.0, 0.0, 0.0));
+
+    // Set initial conditions
+
+    skel->setVelocity(0, w0[0]);
+    skel->setVelocity(1, w0[1]);
+    skel->setVelocity(2, w0[2]);
+
+    skel->setVelocity(3, v0[0]);
+    skel->setVelocity(4, v0[1]);
+    skel->setVelocity(5, v0[2]);
+
+    skel->computeForwardKinematics(true, true, false);
+
+    Vector3d linVel = bodyNode->getWorldLinearVelocity();
+    Vector3d angVel = bodyNode->getWorldAngularVelocity();
+    Vector3d wBody = box->getBodyAngularVelocity();
+
+    double mag = linVel.norm();
+
+    ASSERT_EQ(linVel, v0);
+    ASSERT_EQ(angVel, w0);
+
+//    ASSERT_EQ(v0, bodyNode->GetWorldCoGLinearVel());
+//    ASSERT_EQ(w0, bodyNode->GetWorldAngularVel());
+//    ASSERT_EQ(I0, bodyNode->GetInertial()->GetMOI());
+//    ASSERT_NEAR(link->GetWorldEnergy(), E0, 1e-6);
+  }
+
+  const double simDuration = 10.0;
+  const double t0 = 0.0;
+  // initial linear position in global frame
+  Vector3d p0 = box->getTransform().translation();
+  int steps = ceil(simDuration / _dt);
+
+  DiscreteBodyNode dbody;
+  dbody.initCircular();
+
+  for (int i = 0; i < steps; ++i)
+  {
+    double t_prev = world->getTime() - t0;
+
+    box->setExtForce(
+          (DART_PI/5.0)
+          *(DART_PI/5.0)
+          *Vector3d(cos(DART_PI/5.0*t_prev), -sin(DART_PI/5.0*t_prev), 0.0));
+    world->step();
+//    dbody.updateVelocity();
+    dbody.updateVelocityCircular();
+
+    // current time
+    double t = world->getTime() - t0;
+
+    // linear velocity error
+    Vector3d v = box->getWorldLinearVelocity();
+    Vector3d wBody = box->getBodyAngularVelocity();
+    Vector3d wWorld = box->getWorldAngularVelocity();
+    Vector3d linVelErr = v - (v0 + g*t);
+//    linearVelocityError.InsertData(v - (v0 + g*t));
+//    std::cout << "linVelErr: " << linVelErr.transpose() << std::endl;
+
+    double mag2 = v.norm();
+    double mag3 = box->getBodyVelocity().tail<3>().norm();
+
+    // linear position error
+    Vector3d p = box->getTransform().translation();
+    Vector3d p2 = dbody.mW.translation();
+    Vector3d v2World = dbody.mV.tail<3>();
+    Vector3d exactP(cos(DART_PI/5.0*t), -sin(DART_PI/5.0*t), 0.0);
+    Vector3d posErr = p - exactP;
+    Vector3d posErr2 = p2 - exactP;
+//    linearPositionError.InsertData(p - (p0 + v0 * t + 0.5*g*t*t));
+    std::cout << "posErr: " << posErr.transpose() << std::endl;
+    std::cout << "posErr2: " << posErr2.transpose() << std::endl;
+
+    // angular momentum error
+//    Vector3d H = link->GetWorldInertiaMatrix()*link->GetWorldAngularVel();
+//    angularMomentumError.InsertData((H - H0) / H0mag);
+
+    // energy error
+//    energyError.InsertData((link->GetWorldEnergy() - E0) / E0);
+  }
+
+  delete world;
+}
+
+//==============================================================================
 TEST_F(AccuracyTest, testBoxes)
 {
-  Boxes(0.001, 50, 1, false, false, true);
+//  Boxes(0.001, 50, 1, false, false, true);
+  Boxes2(0.001, 50, 1, false, false, true);
 }
 
 //==============================================================================
